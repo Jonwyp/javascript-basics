d3572a2d5dfa2c3bf20ec7570fe7259af8416ae0
Jonathan Wong <Jonwyp@users.noreply.github.com>
2020-01-09 22:34:57 +0800

    Add my workings to exercise 1 and 2

--- 1-functions.js
+++ 1-functions.js
@@ -2,9 +2,11 @@ const assertEquals = require("./assert-helper");
 
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function that returns 'hello world'
-function hello() {
-  return "hello world";
-}
+/* function hello() {
+   return "hello world";
+ } */
+
+const hello = () => ("hello world");
 
 // Assertions (do not change)
 assertEquals(hello(), "hello world");
@@ -12,8 +14,10 @@ assertEquals(hello(), "hello world");
 /* ---------------------- EXERCISE 2 ---------------------- */
 // Define a function, greet(personName), that returns 'hello specificPersonName'. If no name is supplied, return 'hello world'
 function greet(name) {
-
-}
+  let greet;
+  greet = name ? "hello " + name : "hello world";
+  return greet; 
+};
 
 // Assertions (do not change)
 assertEquals(greet("tom"), "hello tom");
@@ -21,7 +25,9 @@ assertEquals(greet(), "hello world");
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 // Define a function, add2Numbers(num1, num2), to return the sum of 2 values
-
+function add2Numbers(num1, num2) {
+  return num1 + num2;
+};
 
 // Assertions (do not change)
 assertEquals(add2Numbers(1, 1), 2);
@@ -29,7 +35,18 @@ assertEquals(add2Numbers(100, 1), 101);
 
 /* ---------------------- EXERCISE 4 ---------------------- */
 // Define a function, invertCase(someString), that returns the input string with its case inverted
-
+function invertCase(someString) {
+  let newString = "";
+  for (let i = 0; i < someString.length; i++) {
+    if(someString[i] === someString[i].toLowerCase()){
+      newString += someString[i].toUpperCase();
+    }else {
+      newString += someString[i].toLowerCase();
+    }
+  }
+  console.log(newString);  
+  return newString;
+};
 
 // Assertions (do not change)
 assertEquals(invertCase("Hello"), "hELLO");
--- 2-arrays.js
+++ 2-arrays.js
@@ -2,20 +2,37 @@ const assertEquals = require("./assert-helper");
 
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function that returns the sum of an array
+//Method 1: For Loop
 function sum(array) {
-
-}
-
+    let addArray = 0;
+    for (let i = 0; i < array.length; i++) {
+        addArray += array[i];
+    }
+    console.log(addArray);
+    return addArray;
+};
+// Method 2: Reduce method
+const sum1 = (array) => array.reduce((a, b) => a + b, 0)
+console.log(sum1([1, 2, 3, 4]));
 // Assertions (do not change)
 assertEquals(sum([1, 2]), 3);
 assertEquals(sum([1, 2, 3, 4]), 10);
+assertEquals(sum1([1, 2]), 3);
+assertEquals(sum1([1, 2, 3, 4]), 10);
 
 /* ---------------------- EXERCISE 2 ---------------------- */
 // Define a function that returns an array of even numbers
+// Method 1: For annd If
 function evenNumbersOnly(array) {
-
+    let evenArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            evenArray.push(array[i]);
+        }
+    }
+    console.log(evenArray);
+    return evenArray;
 }
-
 // Assertions (do not change)
 assertEquals(evenNumbersOnly([1, 2, 3, 4]), [2, 4]);
 assertEquals(evenNumbersOnly([10, 11, 12]), [10, 12]);
@@ -24,8 +41,15 @@ assertEquals(evenNumbersOnly([10, 11, 12]), [10, 12]);
 // Define a function that returns the sum of even numbers in the array
 // hint: reuse the 2 functions that you defined earlier.
 function sumEvenNumbers(array) {
-
-}
+    let evenArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            evenArray.push(array[i]);
+        }
+    }
+    console.log(evenArray);
+    return evenArray.reduce((a, b) => a + b, 0);
+    };
 
 // Assertions (do not change)
 assertEquals(sumEvenNumbers([1, 2, 3, 4]), 6);
@@ -34,7 +58,9 @@ assertEquals(sumEvenNumbers([10, 11, 12]), 22);
 /* ---------------------- BONUS: EXERCISE 4 ---------------------- */
 // Write a Javascript function to compute the union of two arrays. the returned array should be sorted in ascending order (Sample input: union([1, 2, 3], [100, 2, 1, 10]), expected output: [1, 2, 3, 10, 100])
 function sortedUnion(arr1, arr2) {
-
+const newArray = arr1.concat(arr2);
+return newArray.toString().split(",").sort((a, b) => a - b);
+// return newArray.split(",").sort((a, b) => a - b).join(",")
 }
 
 // Assertions (do not change)
